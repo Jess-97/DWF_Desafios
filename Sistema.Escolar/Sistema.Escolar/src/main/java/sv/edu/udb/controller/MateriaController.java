5b747892c0cac1c50c3db1c80919cdf24ed042c3
Jessica <2021jessicaalvarez@gmail.com>
2026-04-17 21:09:40 -0600

    Implementar endpoints en  MateriaController.java
    Se implementan endpoints CRUD para materia.

--- Sistema.Escolar/Sistema.Escolar/src/main/java/sv/edu/udb/controller/MateriaController.java
+++ Sistema.Escolar/Sistema.Escolar/src/main/java/sv/edu/udb/controller/MateriaController.java
@@ -1,1 +1,36 @@
-JFKJSK
+package sv.edu.udb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Materia;
+import sv.edu.udb.service.MateriaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/materias")
+public class MateriaController {
+
+    @Autowired
+    private MateriaService service;
+
+    @GetMapping
+    public List<Materia> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Materia guardar(@RequestBody Materia m) {
+        return service.guardar(m);
+    }
+
+    @PutMapping("/{id}")
+    public Materia actualizar(@PathVariable Long id, @RequestBody Materia m) {
+        return service.actualizar(id, m);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
+}
