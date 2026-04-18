92bd154224a907ef9dc8b1ef0ace87646dbc98b9
Jessica <2021jessicaalvarez@gmail.com>
2026-04-17 21:01:43 -0600

    Implementar endpoints en  ProfesorController.java
    Se implementan endpoints CRUD para profesor.

--- Sistema.Escolar/Sistema.Escolar/src/main/java/sv/edu/udb/controller/ProfesorController.java
+++ Sistema.Escolar/Sistema.Escolar/src/main/java/sv/edu/udb/controller/ProfesorController.java
@@ -1,1 +1,36 @@
-dfido
+package sv.edu.udb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Profesor;
+import sv.edu.udb.service.ProfesorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/profesores")
+public class ProfesorController {
+
+    @Autowired
+    private ProfesorService service;
+
+    @GetMapping
+    public List<Profesor> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Profesor guardar(@RequestBody Profesor p) {
+        return service.guardar(p);
+    }
+
+    @PutMapping("/{id}")
+    public Profesor actualizar(@PathVariable Long id, @RequestBody Profesor p) {
+        return service.actualizar(id, p);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
+}
